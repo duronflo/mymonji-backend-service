d52d65c89532bcfda27fb38b23d9157e8d71be89
duronflo <flo@durons.net>
2025-09-01 19:34:40 +0200

    Add minor fix

---
+++ packages/backend/src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>MyMonji Visualization</h1>
+      <p>This is where you’ll see the data fed to the chatbot and its responses.</p>
+    </div>
+  );
+}
+
+export default App;
